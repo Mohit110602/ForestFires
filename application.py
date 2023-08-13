bfa3151a09ffdd1ae06f0df5d8a26eba28411c27
Mohit110602 <mohit2014.ma@gmail.com>
2023-08-14 02:43:39 +0530

    first commit

--- application.py
+++ application.py
@@ -4,7 +4,8 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-app = Flask(__name__)
+application = Flask(__name__)
+app=application
 ridge_model=pickle.load(open('models/ridge.pkl','rb'))
 standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
 @app.route('/',methods=['GET'])
@@ -13,7 +14,7 @@ def index():
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='POST':
-        Temperture=float(request.form.get('Temperature'))
+        Temperature=float(request.form.get('Temperature'))
         RH=float(request.form.get('RH'))
         Ws=float(request.form.get('Ws'))
         Rain=float(request.form.get('Rain'))
@@ -22,7 +23,7 @@ def predict_datapoint():
         ISI=float(request.form.get('ISI'))
         Classes=float(request.form.get('Classes'))
         Region=float(request.form.get('Region'))
-        scaled_data=standard_scaler.transform([[Temperture,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        scaled_data=standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
         result=ridge_model.predict(scaled_data)
         return render_template('home.html',result=result[0]) 
     else:
